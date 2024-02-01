e68f9b7b9b05d6d9d00fd3fc34f95c36f42bae28
MarkosComk <markoscomks@gmail.com>
2024-02-01 17:41:06 +0000

    Added right solution to ft_atoi

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,99 +6,55 @@
 /*   By: marsoare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 13:44:43 by marsoare          #+#    #+#             */
-/*   Updated: 2024/01/21 19:00:03 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/02/01 14:58:53 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
 //#include <stdlib.h>
 
-char	*del_spaces(char *str)
+int	whitespaces(char *str, int *ptr_i)
 {
-	while (*str >= 0 && *str <= 42)
-	{
-		str++;
-	}
-	return (str);
-}
+	int	count;
+	int	i;
 
-int	ft_isnumber(char c)
-{
-	if (c >= '0' && c <= '9')
+	i = 0;
+	count = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] && (str[i] == 43 || str[i] == 45))
 	{
-		return (1);
+		if (str[i] == 45)
+			count *= -1;
+		i++;
 	}
-	return (0);
-}
-
-int	ft_isalpha(char c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	return (0);
-}
-
-int	ft_isnegative(char c)
-{
-	if (c == '-')
-	{
-		return (1);
-	}
-	return (0);
+	*ptr_i = i;
+	return (count);
 }
 
 int	ft_atoi(char *str)
 {
+	int	sign;
 	int	result;
-	int	negative;
 	int	i;
 
 	result = 0;
-	negative = 0;
-	i = 0;
-	while (str[i] != 0)
+	sign = whitespaces(str, &i);
+	while (str[i] && str[i] >= 48 && str[i] <= 57)
 	{
-		str = del_spaces (str);
-		if (ft_isalpha(str[i])) 
-			break ;
-		if (str[i] == '-' || str[i] == '+')
-			negative = 1;
-		if (ft_isnumber(str[i]))
-			result = result * 10 + str[i] - 48;
-		if (ft_isnumber(str[i]) && str[i + 1] == ' ')
-			break ;
+		result *= 10;
+		result += str[i] - 48;
 		i++;
 	}
-	if (negative)
-		result *= -1;
+	result *= sign;
 	return (result);
 }
 /*
-int	main(int argc, char **argv)
+int	main(void)
 {
-	int	converted_value;
-	int	atoi_value;
-	
-	converted_value = ft_atoi(argv[1]);
-	atoi_value = atoi(argv[2]);
-	printf("Value of argv in int is: %i\n", converted_value);
-	printf("Value of argv2 in int is: %i\n", atoi_value);
-}
-*/
-/*
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
+	char	*s;
 
-int main() {
-	printf("%i\n", ft_atoi("\t\n\v\f\r      23"));
-	printf("%i\n", ft_atoi("     ------+++++---12341"));
-	printf("%i\n", ft_atoi("     ------+++++---123413"));
-	printf("%i\n", ft_atoi("00000000002147483647"));
-	printf("%i\n", ft_atoi("-2147483648"));
-	printf("%i\n", ft_atoi("a"));
+	s = "   ---+--+01234506789ab567";
+	printf("%d", ft_atoi(s));
 }
 */

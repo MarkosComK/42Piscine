57409f44ebdb830cd4dcc07c983a85e4271bd340
MarkosComK <markoscomks@gmail.com>
2024-02-09 15:02:04 +0000

    solved norminette issue

--- C09/ex00/ft_putstr.c
+++ C09/ex00/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 14:55:38 by marsoare          #+#    #+#             */
-/*   Updated: 2024/02/09 14:57:14 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/02/09 15:01:48 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putstr(char *str)
 	int	i;
 
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
 		write(1, &str[i], 1);
 		i++;

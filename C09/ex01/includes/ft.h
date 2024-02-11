d2b1ffb863fcd95764d345e26e748cfd0ddfec54
MarkosComK <markoscomks@gmail.com>
2024-02-11 15:12:29 +0000

    missing definition solved

--- C09/ex01/includes/ft.h
+++ C09/ex01/includes/ft.h
@@ -6,12 +6,12 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 15:08:01 by marsoare          #+#    #+#             */
-/*   Updated: 2024/02/11 15:11:30 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/02/11 15:12:18 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef
-# define
+#ifndef FT_H
+# define FT_H
 
 void	ft_putchar(char c);
 void	ft_putstr(char *c);

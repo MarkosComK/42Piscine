538c957aab6db8992cbb6f871afd61aa6467b884
MarkosComK <markoscomks@gmail.com>
2024-02-11 15:11:41 +0000

    added definitions

--- C09/ex01/includes/ft.h
+++ C09/ex01/includes/ft.h
@@ -6,8 +6,17 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 15:08:01 by marsoare          #+#    #+#             */
-/*   Updated: 2024/02/11 15:08:04 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/02/11 15:11:30 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef
+# define
 
+void	ft_putchar(char c);
+void	ft_putstr(char *c);
+void	ft_swap(int *a, int *b);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
